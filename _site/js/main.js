6dffd5235ba9fca6b8b0eb58322b63893bd8e309
alexcampillo <acampillo@appdevy.com>
2015-03-09 00:22:39 -0500

    enough for tonight.

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,1 +1,23 @@
+$(document).ready(function() {
+	// $('.btn-primary').on('click', function(e) {
+	// 	e.preventDefault();
+	// 	$('#footer').ScrollTo({
+	// 		duration: 1000,
+	// 		callback: function() {
+	// 			$('.navbar').addClass('animated fadeOutUp')
+	// 			$('#footer').addClass('animated shake')
+	// 		}
+	// 	});
+	// });
 
+	$(window).scroll(function() {
+		if($(this).scrollTop() > 0) {
+			$('.navbar').removeClass('animated fadeInDown').addClass('animated fadeOutUp');
+			// $('.navbar');
+		} if($(this).scrollTop() <= 0) {
+			$('.navbar').removeClass('animated fadeOutUp').addClass('animated fadeInDown');
+			// $('.navbar');
+		}
+	});
+
+});
